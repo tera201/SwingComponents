2e23ee7a5b444ecbd5d450a9854e1f2100c09c95
rnaryshkin <r.naryshkin99@gmail.com>
2025-02-09 00:26:14 +0000

    Updated config

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    java
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+group = "org.tera201"
+version = "1.1.2-SNAPSHOT"
+
+repositories {
+    mavenCentral() // Use Maven Central repository
+}
+
+dependencies {
+    implementation("com.formdev:flatlaf:3.4")
+    implementation("com.formdev:flatlaf-extras:3.4")
+    implementation("com.miglayout:miglayout-swing:11.3")
+    testImplementation("com.formdev:flatlaf-fonts-roboto:2.137") // This is for test scope in Maven
+}
